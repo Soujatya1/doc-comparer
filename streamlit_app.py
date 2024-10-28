03bfa401bdeab944cbc6923130c128c9897c10ab
Soujatya Das <soujatya548@gmail.com>
2024-10-29 02:15:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 import os
 import time
